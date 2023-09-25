57aaf74fdfc13d7021e1780011822755d041b325
martilut <jeykolton@gmail.com>
2023-09-25 03:58:38 +0300

    final version

--- rootApproximator.py
+++ rootApproximator.py
@@ -1,8 +1,31 @@
 from math import cos, sin
 
 
-def separateRoots(func, a, b, stepFactor):
-    pass
+def separateRoots(func, a, b, n):
+    step_counter = 0
+    step = (b - a) / n
+    foundIntervals = list()
+    foundRoots = list()
+
+    start = a
+    end = start + step
+
+    while end <= b:
+        start_value = func(start)
+        end_value = func(end)
+
+        if start_value == 0:
+            foundRoots.append(start_value)
+        elif start_value * end_value < 0:
+            step_counter += 1
+            foundIntervals.append((start, end))
+        else:
+            pass
+
+        start = end
+        end = end + step
+
+    return foundIntervals, foundRoots
 
 
 def printApproximationResults(name, initial_approx, step_counter, approx, last_segment_length, residual):
@@ -35,7 +58,7 @@ def bisectionMethod(func, a, b, eps):
 
     while (local_b - local_a) > 2 * eps:
         step_counter += 1
-        c = (a + b) / 2
+        c = (local_a + local_b) / 2
         if func(c) * func(local_a) <= 0:
             local_b = c
         else:
@@ -43,7 +66,7 @@ def bisectionMethod(func, a, b, eps):
 
     approx = (local_a + local_b) / 2
     initial_approx = (a + b) / 2
-    residual = func(approx)
+    residual = abs(func(approx))
 
     printApproximationResults("Bisection method", initial_approx, step_counter, approx, local_b - local_a, residual)
 
@@ -51,17 +74,18 @@ def bisectionMethod(func, a, b, eps):
 def newtonMethod(func, derivative, second_derivative, a, b, eps):
     step_counter = 0
     initial_approx = x_prev = getStartingValue(a, b, func, second_derivative)
+    x_curr = x_prev - func(x_prev) / derivative(x_prev)
 
-    while True:
+    while abs(x_curr - x_prev) > eps:
         step_counter += 1
         x_curr = x_prev - func(x_prev) / derivative(x_prev)
-        if abs(x_curr - x_prev) < eps:
+        if abs(x_curr - x_prev) <= eps:
             break
         x_prev = x_curr
 
     last_segment_length = x_prev - x_curr
     approx = x_curr
-    residual = func(approx)
+    residual = abs(func(approx))
 
     printApproximationResults('Newton method', initial_approx, step_counter, approx, last_segment_length, residual)
 
@@ -69,19 +93,18 @@ def newtonMethod(func, derivative, second_derivative, a, b, eps):
 def modifiedNewtonMethod(func, derivative, second_derivative, a, b, eps):
     step_counter = 0
     initial_approx = x_prev = x0 = getStartingValue(a, b, func, second_derivative)
+    x_curr = x_prev - func(x_prev) / derivative(x0)
 
-    while True:
+    while abs(x_curr - x_prev) > eps:
         step_counter += 1
         x_curr = x_prev - func(x_prev) / derivative(x0)
-
-        if abs(x_curr - x_prev) < eps:
+        if abs(x_curr - x_prev) <= eps:
             break
-
         x_prev = x_curr
 
     last_segment_length = x_prev - x_curr
     approx = x_curr
-    residual = func(approx)
+    residual = abs(func(approx))
 
     printApproximationResults('Modified Newton method', initial_approx, step_counter, approx, last_segment_length,
                               residual)
@@ -91,13 +114,13 @@ def secantMethod(func, a, b, eps):
     step_counter = 0
     x_prev = a
     x_curr = b
-    x_next = 0
+    # x_next = x_curr - func(x_curr) * (x_curr - x_prev) / (func(x_curr) - func(x_prev))
 
     while True:
         step_counter += 1
         x_next = x_curr - func(x_curr) * (x_curr - x_prev) / (func(x_curr) - func(x_prev))
 
-        if abs(x_next - x_curr) < eps:
+        if abs(x_next - x_curr) <= eps:
             break
 
         x_prev = x_curr
@@ -106,22 +129,43 @@ def secantMethod(func, a, b, eps):
     last_segment_length = x_next - x_curr
     approx = x_next
     initial_approx = (a + b) / 2
-    residual = func(approx)
+    residual = abs(func(approx))
 
     printApproximationResults('Secant method', initial_approx, step_counter, approx, last_segment_length, residual)
 
 
 if __name__ == "__main__":
     print('Численные методы решения нелинейных уравнений')
-    print('eps = 10^(-7)')
-    print('[A ; B] = [-9 ; 1]')
     print('f(x) = 8cos(x) - x - 6')
+    print('[A ; B] = [-9 ; 1]')
+    print('eps = 10^(-7)\n')
 
     function = lambda x: 8 * cos(x) - x - 6
     first_deriv = lambda x: -8 * sin(x) - 1
     second_deriv = lambda x: -8 * cos(x)
 
-    A = float(input("Input A: "))
-    B = float(input("Input B: "))
-    N = int(input("Input N: "))
-    EPS = float(input("Input epsilon: "))
+    a = float(input("Input A: "))
+    b = float(input("Input B: "))
+    n = int(input("Input N: "))
+    eps = float(input("Input epsilon: "))
+
+    print('\n---Root separation---')
+    foundIntervals, foundRoots = separateRoots(function, a, b, n)
+    print(f"\nRoots found during root separation: {len(foundRoots)}\n"
+          f"List of roots: {foundRoots}\n")
+    print(f"Sign changing intervals found during root separation: {len(foundIntervals)}\n"
+          f"List of intervals:")
+    for interval in foundIntervals:
+        print(f" [{interval[0]}, {interval[1]}]")
+    print('\n---Clarification of roots---\n')
+
+    for interval in foundIntervals:
+        print(f"Current interval: [{interval[0]}, {interval[1]}]\n")
+        bisectionMethod(function, interval[0], interval[1], eps)
+        print('\n')
+        newtonMethod(function, first_deriv, second_deriv, a, b, eps)
+        print('\n')
+        modifiedNewtonMethod(function, first_deriv, second_deriv, a, b, eps)
+        print('\n')
+        secantMethod(function, a, b, eps)
+        print('\n')
